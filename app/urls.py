f7a88f7dbcdcdb830baa4735bda6da8bcc7d761a
Jakša Bašić <jaksabasic1@gmail.com>
2015-11-15 16:30:45 +0100

    Integrating the Github API

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, HttpResponse
+import requests, json
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse('Hello World!')
+def profile(request):
+    jsonList = []
+    req = requests.get('https://api.github.com/users/jaksa-b')
+    jsonList.append(json.loads(req.content))
+    parsedData = []
+    userData = {}
+    for data in jsonList:
+        userData['name'] = data['name']
+        userData['email'] = data['email']
+        userData['public_gists'] = data['public_gists']
+        userData['public_repos'] = data['public_repos']
+        userData['avatar_url'] = data['avatar_url']
+        userData['followers'] = data['followers']
+        userData['following'] = data['following']
+
+    parsedData.append(userData)
+    return HttpResponse(parsedData)
